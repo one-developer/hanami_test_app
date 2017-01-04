f732a2cc012688c40a2de4b39a0cde631fc0dfeb
Mikhail Grachev <work@mgrachev.com>
2017-01-04 19:38:07 +0300

    Add project migration

---
+++ db/migrations/20170104163008_create_projects.rb
@@ -0,0 +1,14 @@
+Hanami::Model.migration do
+  change do
+    create_table :projects do
+      primary_key :id
+
+      column :name,  String, null: false
+      column :description, String, null: false
+      column :url, String, null: false
+
+      column :created_at, DateTime, null: false
+      column :updated_at, DateTime, null: false
+    end
+  end
+end
